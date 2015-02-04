60a6d2e4fc04fe0b92989e70b65a4b94f6965b1b
wangxis <wangxis@onid.oregonstate.edu>
2015-02-03 23:28:19 -0800

    That's for today

---
+++ src/loopback.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_All);
+ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>loopback</title>
+</head>
+<body>
+
+<?php
+if (empty($_POST)) {
+	if (empty($_GET){
+		// output empty messeag
+	}
+}
+
+?>
+</body>
+</html>
